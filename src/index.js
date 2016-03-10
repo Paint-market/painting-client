b1883f79690b882eb02fc9b3dda7750cbe0dc163
gaynor-landmark <g.l.landmark@gmail.com>
2016-03-10 14:10:17 +1300

    added canvas

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ var render = require('react-dom').render
 // var { Router, Route, browserHistory }  = require('react-router')
 var App = require('./components/app')
 var domready = require('domready')
+var canvas = require('./components/canvas')
 
 domready(function () {
+  canvas()
+
   render((
-    <App /> 
+    <App />
   ), document.querySelector('#app'))
 })
